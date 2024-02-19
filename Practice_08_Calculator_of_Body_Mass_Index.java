698a35d3d62ac6f8dec1b7447b953f1c47209c0c
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-02-19 20:25:46 +0300

    Practice_08_Calculator_of_Body_Mass_Index.java

--- Practice_08_Calculator_of_Body_Mass_Index.java
+++ Practice_08_Calculator_of_Body_Mass_Index.java
@@ -1,19 +1,19 @@
-import java.util.Scanner;
+//Body Mass Index Calculation
 
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //Body Mass Index Calculation
 
-        //Create variables
+        //Create variables height and weight
         double h, w;
 
         //Define Scanner class
         Scanner inp = new Scanner(System.in);
 
         //Take inputs from user
-        System.out.print("Enter height (m): ");
+        System.out.print("Enter height in meter: ");
         h = inp.nextDouble();
-        System.out.print("Enter weight (kg): ");
+        System.out.print("Enter weight in kg: ");
         w = inp.nextDouble();
 
         //Calculate the BMI
