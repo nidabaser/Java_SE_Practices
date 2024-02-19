c3adcb33e9a879d3e82195f37fccf7b7b581d397
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-02-19 18:45:38 +0300

    .

---
+++ Practice_19_Arithmetic_Operations_and_Operation_Priority.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please enter first number: ");
+        int num1 = s.nextInt();
+        System.out.println("Please enter second number: ");
+        int num2 = s.nextInt();
+        System.out.println("Please enter third number: ");
+        int num3 = s.nextInt();
+        //Calculate given operation
+        //Pay attention for priority of operators
+        int operation = num1 + num2 * num3 - num2;
+        System.out.println("Result is: " + operation);
+    }
+}
