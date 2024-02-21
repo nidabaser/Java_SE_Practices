3b074bb1591a5a9e3b1ad2e613f6c986c6b94f5e
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-02-21 18:12:42 +0300

    Update Practice_02_VAT_Calculator.java

--- Practice_02_VAT_Calculator.java
+++ Practice_02_VAT_Calculator.java
@@ -1,33 +1,32 @@
+//VAT:Value-added Tax Calculator Program
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        //VAT:Value-added Tax Calculator Program
 
         //Create variable
-        double amount_without_VAT, amount_with_VAT, VAT_Amount, VAT_rate_1 = 0.18, VAT_rate_2 = 0.8;
+        double amount, taxAmount, taxRate1 = 0.18, taxRate2 = 0.8;
 
         //Define Scanner class
         Scanner inp = new Scanner(System.in);
 
         //Take inputs from user
         System.out.print("Money amount: ");
-        amount_without_VAT = inp.nextDouble();
+        amount = inp.nextDouble();
 
         //Calculate VAT
-        if(amount_without_VAT > 0 && amount_without_VAT < 1000) {
-            VAT_Amount = (amount_without_VAT * VAT_rate_1);
-            amount_with_VAT = amount_without_VAT + VAT_Amount;
-            System.out.println("Amount without VAT: " + amount_without_VAT);
-            System.out.println("VAT Rate: " + VAT_rate_1);
+        if(amount > 0 && amount < 1000) {
+            taxAmount = (amount * taxRate1);
+            amount = amount + taxAmount;
+            System.out.println("Amount without VAT: " + amount);
+            System.out.println("VAT Rate: " + taxRate1);
         }else{
-            VAT_Amount = (amount_without_VAT * VAT_rate_2);
-            amount_with_VAT = amount_without_VAT + VAT_Amount;
-            System.out.println("Amount without VAT: " + amount_without_VAT);
-            System.out.println("VAT Rate: " + VAT_rate_2);
+            taxAmount = (amount * taxRate2);
+            amount = amount + taxAmount;
+            System.out.println("Amount without VAT: " + amount);
+            System.out.println("VAT Rate: " + taxRate2);
         }
         //Results
-        System.out.println("VAT Amount: " + VAT_Amount);
-        System.out.println("Amount with VAT: " + amount_with_VAT);
+        System.out.println("VAT Amount: " + taxAmount);
+        System.out.println("Amount with VAT: " + amount);
     }
 }
