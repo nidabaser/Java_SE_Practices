e1da5ee29555ea61f2ba04f453554507ef239f1c
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-02-21 23:50:29 +0300

    Practice_Sum_Of_Odd_Numbers.java

---
+++ Loops/Practice_Sum_Of_Odd_Numbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+      
+        int n, sum = 0;
+        Scanner s = new Scanner(System.in);
+
+        do {
+            System.out.print("Enter number: ");
+            n = s.nextInt();
+            if (n % 2 != 0) {
+                sum += n;
+            }
+        } while (n>0);
+      
+        System.out.println("Sum of odd numbers: " + sum);
+    }
+}
