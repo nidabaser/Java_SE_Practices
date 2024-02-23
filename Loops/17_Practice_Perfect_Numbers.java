b18f0b04dbe34c975ba3a3b108f2b1e514a95c8f
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-02-23 10:46:37 +0300

    17_Practice_Perfect_Numbers.java

---
+++ Loops/17_Practice_Perfect_Numbers.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Bir sayı giriniz: ");
+        int sayi = scanner.nextInt();
+
+        if (sayi <= 0) {
+            System.out.println("Lütfen pozitif bir sayı girin.");
+            return;
+        }
+
+        int toplam = 0;
+        for (int i = 1; i <= sayi / 2; i++) {
+            if (sayi % i == 0) {
+                toplam += i;
+            }
+        }
+
+        if (toplam == sayi) {
+            System.out.println(sayi + " mükemmel sayıdır.");
+        } else {
+            System.out.println(sayi + " mükemmel sayı değildir.");
+        }
+    }
+}
