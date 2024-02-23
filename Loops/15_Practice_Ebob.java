133711fe8bda6d63c6284eee57b19735c20849ed
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-02-24 00:19:58 +0300

    15_Practice_Ebob.java

--- Loops/15_Practice_Ebob.java
+++ Loops/15_Practice_Ebob.java
@@ -2,18 +2,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        
         Scanner scan = new Scanner(System.in);
-        int ebob = 0, int max;
+        int gcd = 1;
 
         System.out.print("1. Sayıyı giriniz: ");
         int sayi1 = scan.nextInt();
         System.out.print("2. Sayıyı giriniz: ");
         int sayi2 = scan.nextInt();
-        
-        max = sayi1 * sayi2;
 
+        for (int j = num1; j >= 1; j--) {
+            if (num1 % j == 0 && num2 % j == 0) {
+                gcd = j;
+                break;
+            }
+        }
         
+        System.out.println("GCD = " + gcd);
 
-    
     }
 }
