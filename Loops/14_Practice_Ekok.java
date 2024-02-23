2dfdda4caa5aa72365f4e07706251ea8bf38b9b9
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-02-23 21:40:41 +0300

    Update 14_Practice_Ekok.java

--- Loops/14_Practice_Ekok.java
+++ Loops/14_Practice_Ekok.java
@@ -1,24 +1,21 @@
-import java.util.*;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        
         Scanner scan = new Scanner(System.in);
-        int ekok = 0;
-        int max;
+        
+        int ekok = 0, max;
 
-        System.out.print("1. Sayiyi giriniz: ");
+        System.out.print("First number: ");
         int sayi1 = scan.nextInt();
-        System.out.print("2. Sayiyi giriniz: ");
+        System.out.print("Second number: ");
         int sayi2 = scan.nextInt();
+        
         max = sayi1 * sayi2;
+        
+        // Eğer iki sayı aralarında asal ise ekok bu iki sayının çarpımlarıdır.
 
-/* Eğer iki sayı aralarında asal ise ekoku çarpımlarıdır. Yani iki sayının ekoku maximum çarpımlarıdır.*/
-
-        for(int i = max; i > 0; i--)
-        {
-            if(i % sayi1 == 0 && i % sayi2 == 0) {
-                ekok = i;
-            }
-        }
-        System.out.println("Ekok = " + ekok);
+        
+        
     }
 }
