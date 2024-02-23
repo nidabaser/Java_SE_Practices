cf108c6c93e431db75b2ed1c717a75371f73c91c
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-02-23 18:30:22 +0300

    Update 16_Min_Max_Numbers.java

--- Loops/16_Min_Max_Numbers.java
+++ Loops/16_Min_Max_Numbers.java
@@ -1,37 +1,50 @@
+/**
+ * @author Nida Başer
+ * February 2024
+ */
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Kaç adet sayı gireceksiniz? ");
         int n = scanner.nextInt();
 
-        if (n <= 0) {
+        // Eğer kullanıcı sayı adetini yanlış girmemişse hata mesajı yazdırıp,
+        // While döngüsü ile geçerli bir sayı adeti girene kadar tekrar sayı girmesini istiyoruz
+        while (n <= 0) {
             System.out.println("Lütfen geçerli bir sayı girin.");
-            return;
+            n = scanner.nextInt();
         }
 
-        int sayi;
+        // İlk sayıyı kullanıcıdan alıyoruz.
         System.out.print("1. Sayıyı giriniz: ");
-        sayi = scanner.nextInt();
+        int number = scanner.nextInt();
 
-        int min = sayi;
-        int max = sayi;
+        // İlk sayıyı hem minimum hem de maximum olarak atıyoruz.
+        int min = number;
+        int max = number;
 
+        // Kullanıcıdan gelen diğer sayıları kontrol edip min ve max'ı ona göre güncelliyoruz.
+        // Zaten ilk sayıyı aldığımız için döngüyü 2'den başlatıyoruz.
         for (int i = 2; i <= n; i++) {
             System.out.print(i + ". Sayıyı giriniz: ");
-            sayi = scanner.nextInt();
+            number = scanner.nextInt();
 
-            if (sayi < min) {
-                min = sayi;
+            // Girilen sayı, şu ana kadar görülen en küçük sayıdan küçükse min'i güncelliyoruz.
+            if (number < min) {
+                min = number;
             }
 
-            if (sayi > max) {
-                max = sayi;
+            // Girilen sayı, şu ana kadar görülen en büyük sayıdan büyükse max'ı güncelliyoruz.
+            if (number > max) {
+                max = number;
             }
         }
 
+        // En küçük ve en büyük sayıları ekrana yazdırıyoruz.
         System.out.println("En küçük sayı: " + min);
         System.out.println("En büyük sayı: " + max);
     }
