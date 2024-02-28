0318bd06b3072c263e48e6ea4c9d2ef45c59f608
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-02-28 11:33:55 +0300

    Update 03_Desene_Gore_Metod.java

--- Methods/03_Desene_Gore_Metod.java
+++ Methods/03_Desene_Gore_Metod.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-      
+        // Metodun çağrılması
         sequence(16);
     }
   
     public static void sequence(int num){
-        
+        // Sayı 0'dan küçük eşit ise sayıyı yazdıracak
         if(num <= 0){
             System.out.print(num + " ");
             return;
         }
-      
+        // değilse boşluk bırakıp metod (sayı - 5) değeriyle kendini çağıracak 
         System.out.print(num + " ");
       
         sequence(num - 5); // Metodu yeniden çağırdığımız için
